0d7557924c10e59ff3c2e72abbb4927392d6af56
keerthana0316 <81316031+keerthana0316@users.noreply.github.com>
2025-05-10 00:32:20 +0530

    Add files via upload

---
+++ personal website/script.js
@@ -0,0 +1,7 @@
+// Simple fade-in animation for all sections
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+      el.style.opacity = 1;
+    });
+  });
+  
